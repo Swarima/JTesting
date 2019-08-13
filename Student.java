532d2ce4d93c6527a4dee8fb2e0e48a29742ab54
Swarima15 <38957553+Swarima@users.noreply.github.com>
2019-08-12 22:10:46 -0700

    studentClass

---
+++ Student.java
@@ -0,0 +1,16 @@
+class Student
+{
+	private int rollno;
+	private String name;
+
+	public Student()
+	{
+		rollno=1;
+		name="ashi";
+	}
+	
+	public void show()
+	{
+	System.out.println(name);
+	}
+}
